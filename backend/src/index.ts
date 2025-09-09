7afbed7014f1ca816d721b33d50efc3a67516e50
skandh <skandhjadon02@gmail.com>
2025-09-09 17:01:37 +0530

    new system setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,7 +27,7 @@ const server = createServer(app);
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', "PATCH"],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
